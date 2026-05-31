bdb23ae58e05c778542d9b7137b7591082f6fbbb
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2026-05-31 03:36:36 +0200

    ft: belarusian language

--- src/lib/enums/common.ts
+++ src/lib/enums/common.ts
@@ -1,5 +1,6 @@
 export enum Locales {
   PL = 'pl',
+  BE = 'be',
   UK = 'uk',
 }
 
--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -4,6 +4,7 @@ import { Locales } from '$lib/enums'
 
 export const i18n = async (locale: Locales) => {
   register(Locales.PL, () => import('$lib/i18n/locales/pl.json'))
+  register(Locales.BE, () => import('$lib/i18n/locales/be.json'))
   register(Locales.UK, () => import('$lib/i18n/locales/uk.json'))
 
   return init({
